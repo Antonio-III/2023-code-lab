f7fa7e3feb43e5145b1d7c29d0d8a0e271c34e45
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-03 22:31:44 +0400

    Update Exercise 4 - Largest Number.py
    Fixed a bug where the positional value (first, second,...) doesn't fit the index position of a number

--- Chapter 1 Exercises/Assessment Exercises/Exercise 4 - Largest Number.py
+++ Chapter 1 Exercises/Assessment Exercises/Exercise 4 - Largest Number.py
@@ -1,12 +1,12 @@
 # Write a program to input three numbers and outputs the largest using the multiple if -else statements
 
-three_nums = [] # Initialize a list that will collect the 3 numbers entered
-number_place = ["first", "second", "third"] # Initialize a list to count how many iterations (3) to perform. Gave names to represent the iteration count and their position in the list
+three_nums = {"first":"","second":"","third":""} # Initialize a list that will collect the 3 numbers entered
 
-for n in number_place: # This for loop iterates as much as the amount of items in the list 
+
+for n in three_nums.keys(): # This for loop iterates as much as the amount of items in the list 
     entered_num = int(input(f"Input the {n} number: \n")) # Store the input number into the variable
-    three_nums.append(entered_num) # Add the input number to the first list
-    index = three_nums.index(max(three_nums)) # Store the index value of the highest number in the list. To be used later 
+    three_nums[n] = entered_num
+    index = three_nums.index(max(sorted(three_nums, reverse = True))) # Store the index value of the highest number in the list. To be used later 
 
 #Find the greatest number in the list using multiple if-else statements
 if three_nums[0] > three_nums[1] and three_nums[0] > three_nums[2]: # Both conditions must be true for the first number to be the greatest
@@ -23,6 +23,9 @@ else:
     # All numbers are equal
     if three_nums[0] == three_nums[1] and three_nums[0] == three_nums[2]:
       print(f"All numbers are equal, those being {three_nums[index]}, {three_nums[index+1]}, and {three_nums[index+2]}.")
+        
     # There are 2 greatest numbers
+    elif three_nums[0] == threenums[2]:
+        print(f"The {number_place[index]} and {number_place[index+2]} numbers are both the greatest numbers, being {three_nums[index]}.")
     else:
-      print(f"The {number_place[index]} and {number_place[index+1]} numbers are both the greatest numbers, being {three_nums[index]}.")
+        print(f"The {number_place[index]} and {number_place[index+1]} numbers are both the greatest numbers, being {three_nums[index]}.")
