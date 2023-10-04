232641483321c6eda66a722676b1e89fec425d6c
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-04 13:01:25 +0400

    Update Exercise 4 - Largest Number.py
    Updated to complete the replacement of list to dictionary.
    Have not tested for bugs

--- Chapter 1 Exercises/Assessment Exercises/Exercise 4 - Largest Number.py
+++ Chapter 1 Exercises/Assessment Exercises/Exercise 4 - Largest Number.py
@@ -6,27 +6,24 @@ three_nums = {"first":None,"second":None,"third":None} # Initialize a list that 
 for n in three_nums.keys(): # This for loop iterates as much as the amount of items in the list 
     entered_num = int(input(f"Input the {n} number: \n")) # Store the input number into the variable
     three_nums[n] = entered_num #add the entered nunmbers as the key's value
-entered_nums = list(three_nums.values())
-highest_num_index = entered_nums.index(max(entered_nums))
     
-#Find the greatest number in the list using multiple if-else statements
-if entered_nums[0] > entered_nums[1] and entered_nums[0] > entered_nums[2]: # Both conditions must be true for the first number to be the greatest
-    print(f"The {number_place[index]} number is the greatest number, being {entered_nums[index]}.") # Outputs the greatest number as well as their position in the list
+sorted_nums = dict(sorted(threenums.items(), key = lambda item: item[1], reverse = True)) #Sort the dictionary by their values-pair in descending order by using a custom sorting key. This returns a list-type so we use dict() to convert the list into a dict-type
+
+# Store the variable keys and values to these variables. 'desc' means 'descending'
+desc_num_keys = list(sorted_nums.keys())
+desc_num_values = list(sorted_nums.values())
     
-elif entered_nums[1] > entered_nums[0] and entered_nums[1] > entered_nums[2]:
-    print(f"The {number_place[index]} number is the greatest number, being {entered_nums[index]}.")
+# Find the greatest number in the list using multiple if-else statements.
+if desc_num_values[0] > desc_num_values[1] and desc_num_values[0] > desc_num_values[2]: # Both conditions must be true for the code under to execute
+    print(f"The {highest_num_keys} number is the highest, being {highest_num_values}") # Outputs the greatest number as well as their position in the list
     
-elif entered_nums[2] > entered_nums[0] and entered_nums[2] > entered_nums[1]:
-    print(f"The {number_place[index]} number is the greatest number, being {entered_nums[index]}.")
 
 # In case of all numbers being equal or that there are 2 greatest numbers
 else: 
     # All numbers are equal
-    if entered_nums[0] == entered_nums[1] and entered_nums[0] == entered_nums[2]:
-      print(f"All numbers are equal, those being {three_nums[index]}, {entered_nums[index+1]}, and {entered_nums[index+2]}.")
+    if desc_num_values[0] == desc_num_values[1] and desc_num_values[0] == desc_num_values[2]:
+      print(f"All numbers are equal, those being {desc_num_values[0}, {desc_num_values[1]}, and {desc_num_values[2]}.")
+        
     # There are 2 greatest numbers
-
-    elif entered_nums[0] == entered_nums[2]:
-        print(f"The {number_place[index]} and {number_place[index+2]} numbers are both the greatest numbers, being {entered_nums[index]}.")
     else:
-        print(f"The {number_place[index]} and {number_place[index+1]} numbers are both the greatest numbers, being {entered_nums[index]}.")
+        print(f"The {desc_num_keys[0]} and {desc_num_keys[1]} numbers are both the greatest numbers, being {desc_num_values[0]}.")
