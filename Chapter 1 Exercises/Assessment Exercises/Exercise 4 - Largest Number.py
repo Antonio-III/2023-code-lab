6f508aac8c45526c6abf6f9d0617d485ca939cca
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-04 13:03:42 +0400

    Update Exercise 4 - Largest Number.py
    Fixed bugs

--- Chapter 1 Exercises/Assessment Exercises/Exercise 4 - Largest Number.py
+++ Chapter 1 Exercises/Assessment Exercises/Exercise 4 - Largest Number.py
@@ -7,7 +7,7 @@ for n in three_nums.keys(): # This for loop iterates as much as the amount of it
     entered_num = int(input(f"Input the {n} number: \n")) # Store the input number into the variable
     three_nums[n] = entered_num #add the entered nunmbers as the key's value
     
-sorted_nums = dict(sorted(threenums.items(), key = lambda item: item[1], reverse = True)) #Sort the dictionary by their values-pair in descending order by using a custom sorting key. This returns a list-type so we use dict() to convert the list into a dict-type
+sorted_nums = dict(sorted(three_nums.items(), key = lambda item: item[1], reverse = True)) #Sort the dictionary by their values-pair in descending order by using a custom sorting key. This returns a list-type so we use dict() to convert the list into a dict-type
 
 # Store the variable keys and values to these variables. 'desc' means 'descending'
 desc_num_keys = list(sorted_nums.keys())
@@ -22,7 +22,7 @@ if desc_num_values[0] > desc_num_values[1] and desc_num_values[0] > desc_num_val
 else: 
     # All numbers are equal
     if desc_num_values[0] == desc_num_values[1] and desc_num_values[0] == desc_num_values[2]:
-      print(f"All numbers are equal, those being {desc_num_values[0}, {desc_num_values[1]}, and {desc_num_values[2]}.")
+      print(f"All numbers are equal, those being {desc_num_values[0]}, {desc_num_values[1]}, and {desc_num_values[2]}.")
         
     # There are 2 greatest numbers
     else:
