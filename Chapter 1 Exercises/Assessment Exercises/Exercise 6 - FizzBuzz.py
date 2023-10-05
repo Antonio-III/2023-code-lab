4bd7585912e84e45dc2a3575dd1e92cdb46f19d4
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-05 13:23:02 +0400

    Update Exercise 6 - FizzBuzz.py
    Fixed the order of the conditions

--- Chapter 1 Exercises/Assessment Exercises/Exercise 6 - FizzBuzz.py
+++ Chapter 1 Exercises/Assessment Exercises/Exercise 6 - FizzBuzz.py
@@ -3,13 +3,16 @@
 # Create 'for loop' that iterates in a range function that contains the numbers from 1~100 
 for num in range (1, 101):
 
-  # We use modulo division to check if the 'num' variable would be a multiple of a number (3 in line 7). if the return is 0, it is indeed a multiple
-  if num % 3 == 0:
+  # We use modulo division to check if the 'num' variable would be a multiple of a number (3 and 5 in line 7). if the return is 0, it is indeed a multiple
+  # We put this condition first because we need to make sure that if the number is a multiple of 15, this line (line 8) would execute first
+  if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+    
+  elif num % 3 == 0:
     print("Fizz")
+    
   elif num % 5 == 0:
     print("Buzz")
-  elif num % 5 == 0 and num % 3 == 0:
-    print("FizzBuzz")
 
   # If the iteration fails all the above conditions, it means it is not a multiple of 3 or 5, or both; so we print the number instead
   else:
