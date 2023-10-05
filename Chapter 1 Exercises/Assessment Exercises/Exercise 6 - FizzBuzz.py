6cbd25970deff018b60653c5ee278864a791e8a7
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-05 13:15:12 +0400

    Update Exercise 6 - FizzBuzz.py
    Fixed modulo operator

--- Chapter 1 Exercises/Assessment Exercises/Exercise 6 - FizzBuzz.py
+++ Chapter 1 Exercises/Assessment Exercises/Exercise 6 - FizzBuzz.py
@@ -4,11 +4,11 @@
 for num in range (1, 101):
 
   # We use modulo division to check if the 'num' variable would be a multiple of a number (3 in line 7). if the return is 0, it is indeed a multiple
-  if num %% 3 == 0:
+  if num % 3 == 0:
     print("Fizz")
-  elif num %% 5 == 0:
+  elif num % 5 == 0:
     print("Buzz")
-  elif num %% 5 == 0 and num %% 3 == 0:
+  elif num % 5 == 0 and num % 3 == 0:
     print("FizzBuzz")
 
   # If the iteration fails all the above conditions, it means it is not a multiple of 3 or 5, or both; so we print the number instead
