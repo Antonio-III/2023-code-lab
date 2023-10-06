ff16c9e5961ebc388269488fc2a17f4e2937a55c
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-06 18:57:24 +0400

    Update Exercise 11 - Year Tuples.py
    Updated output descriptions

--- Chapter 1 Exercises/Assessment Exercises/Exercise 11 - Year Tuples.py
+++ Chapter 1 Exercises/Assessment Exercises/Exercise 11 - Year Tuples.py
@@ -15,7 +15,8 @@ print(f"The 3rd-to-the-last number in the tuple is: {year[-3]}! \n") # This prin
 # 3. To reverse the tuple, we will use a slicing notation([::]). Tuples are immutable, so we can't directly modify it. But what we can do is create a copy of the tuple and order sort in a reverse order (in terms of index positions)
 reverse_year = year[::-1]
 
-print(f"This is the original tuple: {year}.\n And this is the reversed tuple: {reverse_year}! \n") # We print the original and the reversed copy 
+print(f"This is the original tuple: {year}.") # We print the original and the reversed copy 
+print(f"And this is the reversed tuple: {reverse_year}. \n")
 
 # 4. We will use '.count()' to see how many elements match the integer '2009'
 times_2009_appeared = year.count(2009)
@@ -23,8 +24,10 @@ print(f"The number of times '2009' has appeared in the tuple is: {times_2009_app
 
 # 5. We will use '.index()' to get the index value of the integer '2018'
 index_2018 = year.index(2018)
-print(f"The index value of '2018' in the tuple is: {index_2018}! \n")
+print(f"The index value of '2018' in the tuple is: {index_2018}!")
+print(f"This means that the number is in the {index_2018}th position! \n")
 
 # 6. We will use len to find out how many elements are in the tuple
 year_len = len(year)
 print(f"The length of the 'year' tuple is: {year_len}!")
+print(f"This means there are {year_len} elements in the tuple!")
