467d88f67cd35422de5fae1477ba3e3452c252be
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-06 13:15:41 +0400

    Update Exercise 9 - Integer List.py
    Updated descriptions
    Updated output format

--- Chapter 1 Exercises/Assessment Exercises/Exercise 9 - Integer List.py
+++ Chapter 1 Exercises/Assessment Exercises/Exercise 9 - Integer List.py
@@ -1,11 +1,11 @@
 # Create an integer list and perform following operations
-# - Create an int list with 10 values
-# - Output the list using a for loop
-# - Output the highest and lowest value
-# - Sort the elements in ascending order
-# - Sort the elements in descending order
-# - Append two elements 
-# - Print the list after appending
+# 1. Create an int list with 10 values
+# 2. Output the list using a for loop
+# 3. Output the highest and lowest value
+# 4. Sort the elements in ascending order
+# 5. Sort the elements in descending order
+# 6. Append two elements 
+# 7. Print the list after appending
 
 # Import these two functions (seed and randint) from the 'random' module. This is so that we can make a list with different variables for every run of the code
 from random import seed
@@ -14,30 +14,35 @@ from random import randint
 # We seed the later random number generator by giving it a random number from 0~10. If we do not give a seed to a random number generator, the seed of this 'randint()' will be the current system time in seconds or milliseconds. This will determine what integer our 'seed()' will get
 seed(randint(0,10))
 
-# Initialize a list with 10 values with random numbers. Random numbers are determined by the integer the 'seed()' (line 15) has, and the integer's seed is determined by the current system time
+# 1. Initialize a list with 10 values with random numbers. Random numbers are determined by the integer the 'seed()' (line 15) has, and the integer's seed is determined by the current system time
 int_list = [randint(0,100) for i in range(10)]
 
-# We output the list using a 'for loop'. It has 10 values so we will loop for 10 times
+# 2. We output the list using a 'for loop'. It has 10 values so we will loop for 10 times
 index_to_print = 0 # We create a variable to be incremented by 1 to be used in the 'for loop'
+print("This is the list as it is:", end = " ")
 for loop in range(10):
-  print(int_list[index_to_print])
+  print(int_list[index_to_print],end = ", ")
   index_to_print+=1
 
-# To output our highest value, we use the 'max()' function. To output our lowest value, we use the 'min()' function
+print("\n") # Create 2 newlines. Output ends with a newline by default. This will make it so that there is 1 empty space gap for every output
+
+# 3. To output our highest value, we use the 'max()' function. To output our lowest value, we use the 'min()' function
 highest_value = max(int_list)
 lowest_value = min(int_list)
-print(f"The highest number in our 'int' list is {highest_value} and the lowest is {lowest_value}!")
+print(f"The highest number in our 'int' list is {highest_value} and the lowest is {lowest_value}! \n")
 
 
-# To sort the elements in ascending order
+# 4. To sort the elements in ascending order
 sorted_list_asc = sorted(int_list)
+print(f"This is the sorted list in asncending order: {sorted_list_asc}. \n")
 
-# To sort the elements in descending order
+# 5. To sort the elements in descending order
 sorted_list_desc = sorted(int_list, reverse = True)
+print(f"This is the sorted list in descending order: {sorted_list_desc}. \n")
 
-# For appending two elements, I will use a the 'randint()' function
+# 6. For appending two elements, I will use a the 'randint()' function
 for x in range(2):
   int_list.append(randint(0,100))
 
-# Then we print the list itself after appending
+# 7. Then we print the list itself after appending
 print(int_list)
