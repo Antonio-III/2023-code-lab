24128cd871a7e2c02a2e266d4672c50438027819
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-06 21:57:26 +0400

    Create Exercise 12 - Area Function.py

---
+++ Chapter 1 Exercises/Assessment Exercises/Exercise 12 - Area Function.py
@@ -0,0 +1,12 @@
+# Code a program to display a menu on the screen that asks if the user wants to
+# 1: Calculate the area of a square
+# 2: Calculate the area of a circle
+# 3: Calculate the area of a triangle 
+
+print("For this exercise, we will calculate the area of a square (1), circle (2), or triangle (3)!")
+
+# Initialize an 'input' variable
+entered_num = (input("Enter a number: "))
+
+if entered_num == '1':
+
