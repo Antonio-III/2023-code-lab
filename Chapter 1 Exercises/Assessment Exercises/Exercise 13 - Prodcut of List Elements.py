ae786a71e37e63b664c922ebe7d68c9ba16c4289
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-07 09:52:53 +0400

    Create Exercise 13 - Prodcut of List Elements.py

---
+++ Chapter 1 Exercises/Assessment Exercises/Exercise 13 - Prodcut of List Elements.py
@@ -0,0 +1,6 @@
+# Write a program that passes a list as an argument to a function. The function should then calculate the product (values multiplied) of the list values and return this value back to the main program.
+
+def list_product(my_list):
+  for i in my_list:
+    index = my_list[i]
+    product = i * i[index]
