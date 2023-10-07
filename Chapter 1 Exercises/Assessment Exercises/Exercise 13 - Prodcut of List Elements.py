187d3fc468f79e7daad2ff98c950634e8bdc71f3
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-07 10:46:50 +0400

    Update Exercise 13 - Prodcut of List Elements.py
    Removed redundant codes for optimization

--- Chapter 1 Exercises/Assessment Exercises/Exercise 13 - Prodcut of List Elements.py
+++ Chapter 1 Exercises/Assessment Exercises/Exercise 13 - Prodcut of List Elements.py
@@ -11,20 +11,15 @@ def list_product(my_list):
     seed(randint(0,10))
 
     # Initialize a counter for the total product. Putting this variable outside the function will not work because since we will call this variable later on (line 19), Python thinks that we are referring to a local variable (a variable inside the function) with the same name, of which doesn't exist, and so will cause an error
-    total_product = 1
+    product = 1
 
     print(f"These are the numbers in that list: {rand_list}! \n")
     
     for i in my_list:
-        # The only reason we have this is for the first iteration. After the first iteration, it functionally is useless since we are assigning the same value twice (see line 26)
-        product = total_product
-
+        
         # We can use a variable's previous value and assign it to the same variable, which will replace the old one
         product = product * i
-
-        # Assign the new value of 'product' to our counter for the next iteration
-        total_product = product
-
+        
     return product
 
 rand_list = [randint(1,10) for i in range(3)]
