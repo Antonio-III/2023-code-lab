8d74372c9c882c5ecd47c7d07459f314f6d5e3da
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-08 11:34:54 +0400

    Update Exercise 08 - Print Pattern.py
    fixed undefined variable

--- Chapter 1 Exercises/Assessment Exercises/Exercise 08 - Print Pattern.py
+++ Chapter 1 Exercises/Assessment Exercises/Exercise 08 - Print Pattern.py
@@ -9,7 +9,7 @@
 # Create the first 'for loop'. This first loop represents the amount of rows we will have (5). We use the arguments '1,6' since we will use the values later on  
 for rows in range(1,6):
   
-  for columns in range(1,x+1): # This nested 'for loop' will represent the printed values in every column. We start from 1 until the current value of 'x'. This means that we will print '1' in the first iteration, then '1 2' in the second and so on. This will be a problem because every 'print()' by-default ends on a newline (\n) 
+  for columns in range(1,rows+1): # This nested 'for loop' will represent the printed values in every column. We start from 1 until the current value of 'x'. This means that we will print '1' in the first iteration, then '1 2' in the second and so on. This will be a problem because every 'print()' by-default ends on a newline (\n) 
     
     print(y, end = " ") # We use the 'end' argument to replace the default 'end' value which is a newline character (\n). This is so that the values we print will all be on the same line, with an empty space ( ) in between. This will create a problem since there is no separation for every iteration, but we will address it later on
   
