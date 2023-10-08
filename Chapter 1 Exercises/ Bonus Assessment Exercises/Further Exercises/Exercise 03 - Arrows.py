29e135130359b87dc9e5f96e3cf1bc013e68c049
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-08 11:32:55 +0400

    Update Exercise 03 - Arrows.py
    removed redundant variables for optimization

--- Chapter 1 Exercises/ Bonus Assessment Exercises/Further Exercises/Exercise 03 - Arrows.py
+++ Chapter 1 Exercises/ Bonus Assessment Exercises/Further Exercises/Exercise 03 - Arrows.py
@@ -1,24 +1,21 @@
-spaces = " "
+# Write a Python program to print the asterisk pattern shown below
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+#    ***  
+#    ***
+#    ***
 
-spaces_count = 4
-
-char = "*"
-
-char_count = 1
-
-for x in range(1,9):
-  
-    if x == 0:
-        print(f"{spaces*spaces_count}{char*x}", end = "")
-        spaces_count -=1
-        char_count +=2
-        
-    elif x <5:
-        print(f"{spaces*spaces_count}{char*x}", end = "")
-        spaces_count -=1
-        char_count +=2
+# Create a loop. This will be the amount of rows we have. We will not create a nested loop because we will not need it. Every iterations end in a new line and inside those iterations are our 'columns'
+for x in range(8):
+    if x < 5:
+        spaces = " " * 3
+        star = "*" * (x * 2 + 1)
+        print(spaces+star)
+        spaces -=1    
     else:
-        char_count, spaces_count = 3,3
-        print(f"{spaces*spaces_count}{char*3}", end = "")        
-
-    print()
+        spaces = " " * 3
+        star  = "*" * 3
+        print(spaces+star)
