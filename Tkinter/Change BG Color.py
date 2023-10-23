a29e0ed50d0bf6dd4a098a2cd32bb416cd90c056
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-23 09:52:38 +0400

    Create Change BG Color.py
    tkinter exercise

---
+++ Tkinter/Change BG Color.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+# create output window
+root = Tk()
+
+# window size
+root.geometry('600x600')
+
+# ---EXERCISE---
+
+# Change the background color
+root.configure(bg=''green')
+
+# Start the app
+root.mainloop()
