5a3f28c0c066a703babd70d6dc625acd9ceb9675
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-24 12:01:06 +0400

    create lesson and exercise entry (item)

---
+++ Tkinter/Lesson 5 - Frames/Frames.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+# Create an output window
+root - tk.Tk()
+
+# Set a title
+root.title('Frames')
+# Set a base window size
+root.geometry('600x600')
+
+leftframe = Frame()
+
+# Start the app
+root.mainloop()
+
