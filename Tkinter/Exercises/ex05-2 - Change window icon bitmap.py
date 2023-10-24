01463bdf749f63f890da56a1399914bb6dcd8435
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-24 10:00:52 +0400

    Update ex05-2 - Change window icon bitmap.py
    complete exercise05-2

---
+++ Tkinter/Exercises/ex05-2 - Change window icon bitmap.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+# Create an output window
+main = tk.Tk()
+
+# Create a base window size
+main.geometry('600x600')
+
+# Change default title
+main.title('Change icon using iconbitmap')
+
+# Create a text at the top
+# Grid default is 0,0. Previous grid row/columns must be made before making a new one, else will display at the lowest uncreated grid 
+tk.Label(main,text='Top text').grid()
+
+# ---CHANGE THE ICON USING ICONBITMAP---
+main.iconbitmap('Images/favicon.ico')
+
+# Start the app
+main.mainloop()
