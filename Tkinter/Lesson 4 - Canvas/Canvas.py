4c208bb8d2ad2923cf4f15c82dfba42335b5d9d0
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-24 10:28:57 +0400

    Update Canvas.py

--- Tkinter/Lesson 4 - Canvas/Canvas.py
+++ Tkinter/Lesson 4 - Canvas/Canvas.py
@@ -1,21 +1,27 @@
-from tkinter import *
+import tkinter as tk
 
 # Create an output window
-root = Tk()
+root = tk.Tk()
 
+# Set a base window size
 root.geometry('600x600')
 
+
+# ---CREATE A CANVAS---
+
 # arguments : (output window, width, height, bg color)
-canvas = Canvas(root, width= 400, height = 400, bg ='black')
+canvas = tk.Canvas(root, width= 400, height = 400, bg ='black')
 
 # Display the object using a 'geometry'. We put in a separate line for clarity
 canvas.pack()
 
 
+# ---IN THE CANVAS, DRAW LINES---
+
 # Draw a rectangle
 
 # X axis
-# For a horizontal line, the Y value will stay the same
+# For a horizontal line, the Y value will stay the same for 1 line
 
 # y = 10 for line 1
 canvas.create_line(10,10,390,10,fill='white',width=2, dash =(1,1))
@@ -25,13 +31,20 @@ canvas.create_line(10,110,390,110,fill='white',width=2)
 
 
 # Y axis
-# Copy and paste the 2 line's startpoints/endpoint as your own startpoint and endpoint
+
+# Y-Line 1 will connect X-Line 1 and 2's start point. Y-L 2 will connect X-L 1 and 2's endpoint
 canvas.create_line(10,10,10,110,fill='white',width=2)
 
 canvas.create_line(390,10,390,110,fill='white',width=2)
 
+
+
+# ---CREATE A RECTANGLE---
+
 # Rectangle
 canvas.create_rectangle(10,200,390,300,fill='red', width=1)
+
+# -----------
 
 
 # Start the app
