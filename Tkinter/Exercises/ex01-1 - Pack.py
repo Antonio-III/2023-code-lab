f02f5ff00ae3e519cffd7dd12f1406ce7ac056d6
Antonio-III <59204403+Antonio-III@users.noreply.github.com>
2023-10-29 17:06:11 +0400

    Update ex01-1 - Pack.py

--- Tkinter/Exercises/ex01-1 - Pack.py
+++ Tkinter/Exercises/ex01-1 - Pack.py
@@ -8,8 +8,8 @@ root.title('Button App') # Title of the app
  
 root.geometry('600x600') # Size of the app
 
-# If side = LEFT/RIGHT will align item vertically. Default: TOP
-# If side = TOP/BOTTOM will align item horizontally
+# If side = LEFT/RIGHT will align item center vertically. Default value: TOP
+# If side = TOP/BOTTOM will align item center horizontally
 
 # If expand = YES, will align the item center horizontally and vertically
 # expand also expands its main axis. And its cross-axis if possible(?)
@@ -23,12 +23,12 @@ Button(root, text = 'B').pack(expand = YES,
 Button(root, text = 'C').pack(side = RIGHT, 
                               expand = YES,
                               fill = NONE,
-                              anchor = NE,)
+                              anchor = NE)
 
 
-Button(root, text = 'D').pack(side = BOTTOM, 
+Button(root, text = 'D').pack(side = RIGHT, #RIGHT/BOTTOM have the same effect
                               expand = NO,
-                              fill = BOTH)
+                              fill = NONE)
 
 
 # This will run the codes written before this code
